df8fa8f584462df92c23d6ece650b0ef99a15576
Stefano Magni <nori.ste.magni@gmail.com>
2023-02-23 18:11:07 +0100

    Create a react+vite integrated monorepo

---
+++ apps/feature-first-hasura-console-poc/src/app/app.tsx
@@ -0,0 +1,16 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="feature-first-hasura-console-poc" />
+
+      <div />
+    </>
+  );
+}
+
+export default App;
